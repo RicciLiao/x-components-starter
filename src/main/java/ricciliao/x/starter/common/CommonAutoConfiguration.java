5f61aa7c430bff48121faf90337276d413f14da8
ricciliao <riccix@163.com>
2025-09-08 14:19:50 +0800

    feat: 1. add HandlerExceptionResolver; 2. use HandlerMethodReturnValueHandler instead of ResponseAdvice.

--- src/main/java/ricciliao/x/starter/common/CommonAutoConfiguration.java
+++ src/main/java/ricciliao/x/starter/common/CommonAutoConfiguration.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.annotation.Nonnull;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
@@ -14,23 +15,30 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import ricciliao.x.component.context.TypedLifecycleBeanPostProcessor;
-import ricciliao.x.component.response.ResponseAdvice;
+import ricciliao.x.component.response.ResponseExceptionResolver;
 import ricciliao.x.component.response.ResponseHttpMessageConverter;
 import ricciliao.x.component.response.ResponseModule;
+import ricciliao.x.component.response.ResponseValueHandler;
 import ricciliao.x.component.utils.SpringBeanUtils;
 import ricciliao.x.starter.PropsAutoConfiguration;
 
-import java.util.Collections;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.TimeZone;
 
 @PropsAutoConfiguration(
         properties = CommonAutoProperties.class
 )
-public class CommonAutoConfiguration {
+public class CommonAutoConfiguration implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -5872892465550813182L;
 
     public CommonAutoConfiguration(@Autowired ApplicationContext applicationContext,
                                    @Autowired CommonAutoProperties props,
@@ -71,41 +79,58 @@ public class CommonAutoConfiguration {
             this.objectMapper = objectMapper;
         }
 
+        @Bean
+        public ResponseHttpMessageConverter responseHttpMessageConverter() {
+
+            return new ResponseHttpMessageConverter(objectMapper);
+        }
+
         @Override
         public void extendMessageConverters(@Nonnull List<HttpMessageConverter<?>> converters) {
             WebMvcConfigurer.super.extendMessageConverters(converters);
-            converters.addFirst(new ResponseHttpMessageConverter(objectMapper));
+            converters.addFirst(responseHttpMessageConverter());
         }
 
-    }
+        @Override
+        public void extendHandlerExceptionResolvers(@Nonnull List<HandlerExceptionResolver> resolvers) {
+            WebMvcConfigurer.super.extendHandlerExceptionResolvers(resolvers);
+            resolvers.addFirst(new ResponseExceptionResolver(responseHttpMessageConverter()));
+        }
 
-    @ConditionalOnClass(WebMvcConfigurer.class)
-    @Bean
-    public TypedLifecycleBeanPostProcessor<RequestMappingHandlerAdapter> adapterPostProcessor() {
+        @Bean
+        public TypedLifecycleBeanPostProcessor<RequestMappingHandlerAdapter> adapterPostProcessor() {
 
-        return TypedLifecycleBeanPostProcessor.processor(new TypedLifecycleBeanPostProcessor.LifecycleProcessor<>() {
+            return TypedLifecycleBeanPostProcessor.processor(new TypedLifecycleBeanPostProcessor.LifecycleProcessor<>() {
 
-            @Override
-            public boolean supports() {
+                @Override
+                public boolean supports() {
 
-                return true;
-            }
+                    return true;
+                }
 
-            @Override
-            public Class<RequestMappingHandlerAdapter> getBeanType() {
+                @Override
+                public Class<RequestMappingHandlerAdapter> getBeanType() {
 
-                return RequestMappingHandlerAdapter.class;
-            }
+                    return RequestMappingHandlerAdapter.class;
+                }
 
-            @Override
-            public RequestMappingHandlerAdapter beforeInitialization(@Nonnull RequestMappingHandlerAdapter bean,
-                                                                     @Nonnull String beanName) {
-                bean.setResponseBodyAdvice(Collections.singletonList(new ResponseAdvice()));
+                @Override
+                public RequestMappingHandlerAdapter beforeInitialization(@Nonnull RequestMappingHandlerAdapter bean,
+                                                                         @Nonnull String beanName) {
+                    List<HandlerMethodReturnValueHandler> originalHandlers = bean.getReturnValueHandlers();
+                    List<HandlerMethodReturnValueHandler> newHandlers = new ArrayList<>();
+                    if (CollectionUtils.isNotEmpty(originalHandlers)) {
+                        newHandlers.addAll(originalHandlers);
+                    }
+                    newHandlers.addFirst(new ResponseValueHandler(responseHttpMessageConverter()));
+                    bean.setReturnValueHandlers(newHandlers);
 
-                return bean;
-            }
+                    return bean;
+                }
 
-        });
+            });
+        }
+
     }
 
 }
