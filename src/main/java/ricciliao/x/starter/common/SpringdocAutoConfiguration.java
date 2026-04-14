d76af192dbc01719cff92052a028c9bb6f68b80a
ricciliao <riccix@163.com>
2026-04-14 16:20:19 +0800

    feat: support json RequestPart in swagger-ui.

--- src/main/java/ricciliao/x/starter/common/SpringdocAutoConfiguration.java
+++ src/main/java/ricciliao/x/starter/common/SpringdocAutoConfiguration.java
@@ -7,16 +7,29 @@ import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Encoding;
 import io.swagger.v3.oas.models.parameters.HeaderParameter;
 import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.parameters.RequestBody;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestPart;
 import ricciliao.x.starter.PropsAutoConfiguration;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @PropsAutoConfiguration(
         properties = CommonAutoProperties.class,
@@ -33,7 +46,6 @@ public class SpringdocAutoConfiguration {
         this.commonProps = commonProps;
         this.buildProps = buildProps;
     }
-
 
     @Bean
     public OpenAPI api() {
@@ -88,6 +100,38 @@ public class SpringdocAutoConfiguration {
                                             }
                                         }
                                 );
+    }
+
+    @Bean
+    public OperationCustomizer requestPart2JsonCustomizer() {
+
+        return (operation, handlerMethod) -> {
+            RequestBody requestBody = operation.getRequestBody();
+            if (Objects.isNull(requestBody)
+                || Objects.isNull(requestBody.getContent())
+                || Objects.isNull(requestBody.getContent().get(MediaType.MULTIPART_FORM_DATA_VALUE))
+                || Objects.nonNull(requestBody.getContent().get(MediaType.MULTIPART_FORM_DATA_VALUE).getEncoding())) {
+
+                return operation;
+            }
+
+            List<RequestPart> requestPartList =
+                    Arrays.stream(handlerMethod.getMethod().getParameters())
+                            .filter(parameter -> parameter.isAnnotationPresent(RequestPart.class)
+                                                 && Serializable.class.isAssignableFrom(parameter.getType()))
+                            .map(parameter -> parameter.getAnnotation(RequestPart.class))
+                            .toList();
+            if (CollectionUtils.isEmpty(requestPartList)) {
+
+                return operation;
+            }
+
+            Map<String, Encoding> encodingMap = new LinkedHashMap<>();
+            requestPartList.forEach(requestPart -> encodingMap.put(requestPart.name(), new Encoding().contentType(MediaType.APPLICATION_JSON_VALUE)));
+            requestBody.getContent().get(MediaType.MULTIPART_FORM_DATA_VALUE).encoding(encodingMap);
+
+            return operation;
+        };
     }
 
 }
